bf0ddc3cd5a1537dd1449f561d76dc903d3b08bb
Alice <wangyijth@163.com>
2021-12-17 20:53:08 +0800

    Create btrain.py

---
+++ btrain.py
@@ -0,0 +1,164 @@
+import os
+import math
+import argparse
+
+import torch
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+import torch.optim.lr_scheduler as lr_scheduler
+
+from a_model_stage2 import ShuffleNetV2
+from my_dataset import MyDataSet
+from utils import read_split_data, train_one_epoch, evaluate
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    print(args)
+    print('Start Tensorboard with "tensorboard --logdir=runs", view at http://localhost:6006/')
+    tb_writer = SummaryWriter()
+    if os.path.exists("./weights_eac2") is False:
+        os.makedirs("./weights_eac2")
+
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
+
+    data_transform = {
+        "train": transforms.Compose([transforms.Resize((224, 244)),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize((244, 244)),
+                                   #transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+    # 实例化训练数据集
+    train_dataset = MyDataSet(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=data_transform["train"])
+
+    # 实例化验证数据集
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
+
+    batch_size = args.batch_size
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=nw,
+                                               collate_fn=train_dataset.collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
+                                             num_workers=nw,
+                                             collate_fn=val_dataset.collate_fn)
+
+    # 如果存在预训练权重则载入
+    #model = shufflenet_v2_x1_5(num_classes=args.num_classes).to(device)
+    model = ShuffleNetV2(num_classes=args.num_classes,
+                         stages_repeats=[3, 7, 3],
+                         stages_out_channels=[24, 176, 352, 704, 1024]).to(device)
+    if args.weights != "":
+        if os.path.exists(args.weights):
+            weights_dict = torch.load(args.weights, map_location=device)
+            load_weights_dict = {k: v for k, v in weights_dict.items()
+                                 if model.state_dict()[k].numel() == v.numel()}
+            print(model.load_state_dict(load_weights_dict, strict=False))
+        else:
+            raise FileNotFoundError("not found weights file: {}".format(args.weights))
+
+    # 是否冻结权重
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除最后的全连接层外，其他权重全部冻结
+            if "fc" not in name:
+                para.requires_grad_(False)
+
+    pg = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.SGD(pg, lr=args.lr, momentum=0.6, weight_decay=5E-5)
+    # Scheduler https://arxiv.org/pdf/1812.01187.pdf
+    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+
+    for epoch in range(args.epochs):
+        # train
+        mean_loss, train_acc = train_one_epoch(model=model,
+                                    optimizer=optimizer,
+                                    data_loader=train_loader,
+                                    device=device,
+                                    epoch=epoch)
+        print("\n")
+        print("[epoch {}] train_accuracy: {}".format(epoch, round(train_acc, 3)))
+        scheduler.step()
+
+        # validate
+        val_acc, val_loss = evaluate(model=model,
+                                    data_loader=val_loader,
+                                    device=device,
+                                    optimizer=optimizer)
+
+        print("[epoch {}] val_accuracy: {} val_loss_out: {}".format(epoch, round(val_acc, 3),round(val_loss.item(),3)))
+        tags = ["train_loss", "val_acc", "learning_rate", "val_loss", "train_acc"]
+        tb_writer.add_scalar(tags[0], mean_loss, epoch)
+        tb_writer.add_scalar(tags[1], val_acc, epoch)
+        tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch)
+        tb_writer.add_scalar(tags[3], val_loss, epoch)
+        tb_writer.add_scalar(tags[4], train_acc, epoch)
+
+        torch.save(model.state_dict(), "./weights_eac2/model-{}.pth".format(epoch))
+
+        # 将模型写入tensorboard
+        init_img = torch.zeros((1, 3, 224, 224), device=device)  # 参数：模型输入tensor和使用训练的设备
+        tb_writer.add_graph(model, init_img)  # 利用模型和零矩阵创建网络的结构图
+
+'''
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=5)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch-size', type=int, default=4)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--lrf', type=float, default=0.1)
+
+    # 数据集所在根目录
+    # http://download.tensorflow.org/example_images/flower_photos.tgz
+    parser.add_argument('--data-path', type=str,
+                        default="D:\pycharm_project\data\\flower_data\\flower_photos")
+
+    # shufflenetv2_x1.0 官方权重下载地址
+    # https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth
+    parser.add_argument('--weights', type=str, default='',
+                        help='initial weights path')
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+
+    main(opt)
+'''
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=4)
+    parser.add_argument('--epochs', type=int, default=120)
+    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--lr', type=float, default=0.1)
+    parser.add_argument('--lrf', type=float, default=0.0001)
+
+    # 数据集所在根目录
+    parser.add_argument('--data-path', type=str, default="E:\project\\network\datasets\yumi\data\\train")
+
+    parser.add_argument('--weights', type=str, default='',
+                        help='initial weights path')
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+    main(opt)

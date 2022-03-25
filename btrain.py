73d15e85a7ccad9c5fb27a0d7c84239740d6d0ac
Alice <wangyijth@163.com>
2022-03-25 21:02:57 +0800

    Update btrain.py

--- btrain.py
+++ btrain.py
@@ -114,7 +114,7 @@ def main(args):
         tb_writer.add_scalar(tags[3], val_loss, epoch)
         tb_writer.add_scalar(tags[4], train_acc, epoch)
 
-        torch.save(model.state_dict(), "./weights_eac2/model-{}.pth".format(epoch))
+        #torch.save(model.state_dict(), "./weights_eac2/model-{}.pth".format(epoch))
 
         # 将模型写入tensorboard
         init_img = torch.zeros((1, 3, 224, 224), device=device)  # 参数：模型输入tensor和使用训练的设备
@@ -148,7 +148,7 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--num_classes', type=int, default=24)
-    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batch-size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=0.1)
     parser.add_argument('--lrf', type=float, default=0.0001)
@@ -156,7 +156,7 @@ if __name__ == '__main__':
     # 数据集所在根目录
     parser.add_argument('--data-path', type=str, default="D:\dataset\\fruits-360-original-size\\fruits-360-original-size\Training")
 
-    parser.add_argument('--weights', type=str, default='MobileNetV2.pth',
+    parser.add_argument('--weights', type=str, default='',#MobileNetV2.pth
                         help='initial weights path')
     parser.add_argument('--freeze-layers', type=bool, default=False)
     parser.add_argument('--device', default='cuda', help='device id (i.e. 0 or 0,1 or cpu)')
